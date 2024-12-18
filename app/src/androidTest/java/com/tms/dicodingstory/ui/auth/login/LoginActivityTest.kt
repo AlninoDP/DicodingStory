aeb1040ea422bb3ea7aef52df27e4126eba7cfcf
Aelin Putra <alninodp@gmail.com>
2024-12-18 13:53:32 +0700

    added ui test for login

--- app/src/androidTest/java/com/tms/dicodingstory/ui/auth/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/tms/dicodingstory/ui/auth/login/LoginActivityTest.kt
@@ -1,25 +1,21 @@
 package com.tms.dicodingstory.ui.auth.login
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.tms.dicodingstory.utils.EspressoIdlingResource
 import org.junit.After
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.tms.dicodingstory.R
-import com.tms.dicodingstory.utils.wrapEspressoIdlingResource
 import org.hamcrest.core.AllOf
+
 
 class LoginActivityTest {
 
@@ -29,6 +25,7 @@ class LoginActivityTest {
     @Before
     fun setup() {
         IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+
     }
 
     @After
@@ -39,26 +36,29 @@ class LoginActivityTest {
 
     @Test
     fun loginSuccess() {
-//        onView(withId(R.id.email_edit_text)).perform(
-//            typeText("valin@gmail.com"),
-//            closeSoftKeyboard()
-//        )
 
-        // Alternatively, directly target the child EditText:
+        // Write email to the edit text
         onView(
             AllOf.allOf(
-            isDescendantOfA(withId(R.id.login_email_form)),
-            withId(R.id.email_edit_text)
-        )).perform(typeText("test@example.com"), closeSoftKeyboard())
+                isDescendantOfA(withId(R.id.login_email_form)),
+                withId(R.id.email_edit_text)
+            )
+        ).perform(typeText("valin@gmail.com"), closeSoftKeyboard())
 
-        onView(withId(R.id.login_password_form)).perform(typeText("123qweasd"), closeSoftKeyboard())
+        // Write password to the edit text
+        onView(
+            AllOf.allOf(
+                isDescendantOfA(withId(R.id.login_password_form)),
+                withId(R.id.password_edit_text)
+            )
+        ).perform(typeText("123qweasd"), closeSoftKeyboard())
 
+        // Click the sign in button and wait
         onView(withId(R.id.btn_sign_in)).perform(click())
-
-        onView(withId(R.id.login_progress_bar)).check(matches(isDisplayed()))
-
-        onView(withId(R.id.home_main)).check(matches(isDisplayed()))
+        // Check if the RecyclerView in HomeActivity is displayed
+        onView(withId(R.id.home_rv)).check(matches(isDisplayed()))
 
     }
 
 }
+
--- app/src/main/java/com/tms/dicodingstory/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/tms/dicodingstory/ui/auth/login/LoginViewModel.kt
@@ -12,39 +12,44 @@ import com.tms.dicodingstory.data.User
 import com.tms.dicodingstory.data.exception.ApiException
 import com.tms.dicodingstory.data.remote.response.ErrorResponse
 import com.tms.dicodingstory.data.remote.response.LoginResponse
+import com.tms.dicodingstory.utils.wrapEspressoIdlingResource
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
 class LoginViewModel(
     private val mainRepository: MainRepository,
     private val preferencesRepository: PreferencesRepository
-) : ViewModel(){
+) : ViewModel() {
 
     private val _loginResponse = MutableLiveData<Result<LoginResponse>>()
     val loginResponse: LiveData<Result<LoginResponse>> get() = _loginResponse
 
-    fun login(email:String, password:String){
+    fun login(email: String, password: String) {
         viewModelScope.launch {
             _loginResponse.value = Result.Loading(true)
-            try {
-                val response = mainRepository.login(email, password)
-                val userId = response.loginResult.userId
-                val name = response.loginResult.name
-                val token = response.loginResult.token
-                val user = User(userId,name,token)
-                preferencesRepository.saveUserToken(user)
-                _loginResponse.value = Result.Success(response)
-            } catch (e: HttpException){
-                val errorMessageJson = e.response()?.errorBody()?.string()
-                val errorBody = Gson().fromJson(errorMessageJson, ErrorResponse::class.java)
-                val errorMessage = errorBody.message ?: "Unknown error occurred"
-                _loginResponse.value = Result.Failure(ApiException(errorMessage))
-            } catch (e: Exception){
-                _loginResponse.value = Result.Failure(e)
-            }finally {
-                _loginResponse.value = Result.Loading(false)
+
+            wrapEspressoIdlingResource {
+                try {
+                    val response = mainRepository.login(email, password)
+                    val userId = response.loginResult.userId
+                    val name = response.loginResult.name
+                    val token = response.loginResult.token
+                    val user = User(userId, name, token)
+                    preferencesRepository.saveUserToken(user)
+                    _loginResponse.value = Result.Success(response)
+                } catch (e: HttpException) {
+                    val errorMessageJson = e.response()?.errorBody()?.string()
+                    val errorBody = Gson().fromJson(errorMessageJson, ErrorResponse::class.java)
+                    val errorMessage = errorBody.message ?: "Unknown error occurred"
+                    _loginResponse.value = Result.Failure(ApiException(errorMessage))
+                } catch (e: Exception) {
+                    _loginResponse.value = Result.Failure(e)
+                } finally {
+                    _loginResponse.value = Result.Loading(false)
+                }
             }
         }
+
     }
 
 }
